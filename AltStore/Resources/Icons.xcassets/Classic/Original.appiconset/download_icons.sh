e6af614e2895e7883f7d691fef8e6a3cfc9e14a2
R3ALCYB3R <rivalscompletion@gmail.com>
2026-06-19 01:21:57 -0400

    Update download_icons.sh

--- AltStore/Resources/Icons.xcassets/Classic/Original.appiconset/download_icons.sh
+++ AltStore/Resources/Icons.xcassets/Classic/Original.appiconset/download_icons.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd AltStore/Resources/Icons.xcassets/Classic/Original.appiconset/
 curl -L -o "Group 23.png" "https://i.imgur.com/3bkEeCW.png"
 curl -L -o "Group 23_120.png" "https://i.imgur.com/3bkEeCW.png"
 curl -L -o "Group 23_180.png" "https://i.imgur.com/3bkEeCW.png"
