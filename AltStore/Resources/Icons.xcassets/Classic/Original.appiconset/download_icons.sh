c3765052a441a5b67a41bf7570e93cf95ed000a7
R3ALCYB3R <rivalscompletion@gmail.com>
2026-06-19 01:19:49 -0400

    Create download_icons.sh

---
+++ AltStore/Resources/Icons.xcassets/Classic/Original.appiconset/download_icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd AltStore/Resources/Icons.xcassets/Classic/Original.appiconset/
+curl -L -o "Group 23.png" "https://i.imgur.com/3bkEeCW.png"
+curl -L -o "Group 23_120.png" "https://i.imgur.com/3bkEeCW.png"
+curl -L -o "Group 23_180.png" "https://i.imgur.com/3bkEeCW.png"
